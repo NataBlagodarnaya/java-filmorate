76f0734eea3b011fef8afbc85eb91f0cba212d0a
Blagodarnaya <natalya-blagodarnaya@yandex.ru>
2026-05-31 14:14:12 +0300

    1 пробел добавила

--- src/main/java/ru/yandex/practicum/filmorate/exception/DuplicatedDataException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/DuplicatedDataException.java
@@ -4,7 +4,7 @@ package ru.yandex.practicum.filmorate.exception;
 //import org.springframework.web.bind.annotation.ResponseStatus;
 
 //@ResponseStatus(HttpStatus.CONFLICT) - по-хорошему тут правильно код 409, пришлось закомитить чтобы тесты из ТЗ в Postman все прошли
-public class DuplicatedDataException extends RuntimeException{
+public class DuplicatedDataException extends RuntimeException {
     public DuplicatedDataException(String message) {
         super(message);
     }
